b72e6ea9977865a03fb1ec19072e544109a70812
Isaac <isaac@grphcrtv.com>
2026-02-02 09:58:51 +1100

    fix guild join doubling and role sorting

--- src/data/threads.test.ts
+++ src/data/threads.test.ts
@@ -1,17 +1,17 @@
 import { expect, test } from "bun:test";
-import { formatUsername } from "./threads";
 import { UnicodePeriod } from "../style";
+import { formatUsername } from "./threads";
 
 test("usernames are formatted correctly", () => {
-  const values = {
-    noryasta: "noryasta",
-    "jules.jpg": `jules${UnicodePeriod}jpg`,
-    ___thatonegamer: "___thatonegamer",
-    _thegodminecraft_: "_thegodminecraft_",
-    "": "unknown",
-  };
+	const values = {
+		noryasta: "noryasta",
+		"jules.jpg": `jules${UnicodePeriod}jpg`,
+		___thatonegamer: "___thatonegamer",
+		_thegodminecraft_: "_thegodminecraft_",
+		"": "unknown",
+	};
 
-  for (const [input, expectation] of Object.entries(values)) {
-    expect(formatUsername(input)).toBe(expectation);
-  }
+	for (const [input, expectation] of Object.entries(values)) {
+		expect(formatUsername(input)).toBe(expectation);
+	}
 });
