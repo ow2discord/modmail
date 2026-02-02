3e95c3d6a699bd3004cfaea77b949985c05b0d77
Isaac <isaac@grphcrtv.com>
2026-02-03 00:05:37 +1100

    drop deps and add migration commands

--- src/plugins/info.ts
+++ src/plugins/info.ts
@@ -1,8 +1,61 @@
-import type { DiscordAPIError, Guild, GuildMember } from "discord.js";
+import {
+  EmbedBuilder,
+  type DiscordAPIError,
+  type Guild,
+  type GuildMember,
+} from "discord.js";
 import type { ModuleProps } from "../plugins";
 import { getMainGuilds } from "../utils";
+import { Emoji, Spacing } from "../style";
 
 export default ({ commands, bot }: ModuleProps) => {
+  commands.addInboxThreadCommand(
+    "fakeclose",
+    "",
+    async (msg, _args, thread) => {
+      if (!thread || !msg.channel.isSendable()) return;
+
+      const user = await bot.users.fetch(thread.user_id);
+      if (!user) return;
+      const author = user;
+
+      // author name/id
+      // participant mod names
+      // closing mod name
+      // messages sent from user
+      // messages sent to user
+      // internal messages
+      // loglink
+      // time open
+
+      const closeTime = new Date();
+      const embed = new EmbedBuilder();
+      embed.setTitle(`Closed thread with ${user.username}`);
+      embed.addFields([
+        {
+          value: `Opened by <@${author.id}>`,
+          name: `<t:${Math.round(closeTime.getTime() / 1000)}:S>`,
+          inline: true,
+        },
+        {
+          name: "Open for 11h30m",
+          value: `Closed by <@${user.id}>`,
+          inline: true,
+        },
+        {
+          name: `Messages ${Emoji.Roles.LFGTool} 8${Spacing.Doublespace}•${Spacing.Doublespace}${Emoji.Roles.Moderator}4`,
+          value: `<@164564849915985922> (8), <@204084691425427466> (3), and <@166767825350819840> (172)`,
+        },
+      ]);
+      embed.setTimestamp(new Date());
+
+      thread.postSystemMessage({
+        content: "",
+        embeds: [embed],
+      });
+    },
+    {},
+  );
   commands.addInboxThreadCommand(
     "header",
     "",
